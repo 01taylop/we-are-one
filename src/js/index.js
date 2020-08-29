39aba75646bc194ae4bb694f79ecbca5803e4b6b
Patrick Taylor <01taylop@users.noreply.github.com>
2020-08-29 18:26:10 +0100

    Added scss mixins, defaults, variables and more 👨‍🎨 (#5)

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,7 @@ const App = () => (
     <header className='app-header'>
       <img src={logo} className='app-logo' alt='logo'/>
       <p>Coming Soon.</p>
-      <a className='app-link' href='https://github.com/01taylop/we-are-one' target='_blank' rel='noopener noreferrer'>View on GitHub</a>
+      <a href='https://github.com/01taylop/we-are-one' target='_blank' rel='noopener noreferrer'>View on GitHub</a>
     </header>
   </div>
 )
