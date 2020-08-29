9fc193a1a21dee1ee4d60922693e20ddd6e947a5
Patrick Taylor <01taylop@users.noreply.github.com>
2020-08-29 17:40:10 +0100

    Added scss 🎨 (#4)

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import logo from './logo.svg'
-import './App.css'
+import logo from '../assets/logo.svg'
+import '../scss/app.scss'
 
 const App = () => (
   <div className='app'>
